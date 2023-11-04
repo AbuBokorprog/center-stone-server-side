5318ae29eb47012b8b3aa85d05de8b0b68ecd5a3
AbuBokorprog <abubokor1066@gmail.com>
2023-11-04 17:01:56 +0600

    resolve

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 const jewelry = require("./model/jewelry");
 const user = require("./model/User");
 const router = express.Router();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
