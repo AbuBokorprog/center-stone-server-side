64cc927608f1d9ee1a76d2441f0e2edaf5bab842
AbuBokorprog <abubokor1066@gmail.com>
2023-11-04 22:32:57 +0600

    particular email get

--- index.js
+++ index.js
@@ -38,6 +38,18 @@ router.post("/users", async (req, res) => {
   }
 });
 
+router.get("/users/:email", async (req, res) => {
+  try {
+    const email = req.params.email;
+    const result = await user.find({ email: email });
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({
+      Error: "This is a user server error.",
+    });
+  }
+});
+
 router.post("/jewelry", async (req, res) => {
   try {
     const newJewelry = new jewelry(req.body);
